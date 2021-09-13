c6e5c157b3b734963a3423e410e2821a64608d93
add33m <48681209+add33m@users.noreply.github.com>
2021-09-13 17:10:55 +0200

    Mostly completed labb4

--- labb4/telefonbok.py
+++ labb4/telefonbok.py
@@ -9,6 +9,10 @@ class Phonebook:
       program = cmd[0]                            # Ta ut första ordet som kommandot som ska användas
       args = len(cmd) > 1 and cmd.pop(0) and cmd          # Ta bort första ordet och låt resten vara en lista av argument
 
+      # Hantera quit
+      if program == "quit":
+        break
+
       # Inget vanligt kommando kan köras utan argument
       if args:
         # Testa inputten mot alla möjliga kommandon
@@ -18,17 +22,28 @@ class Phonebook:
           print(self.lookup(args))
         elif program == "alias":
           print(self.alias(args))
+        elif program == "change":
+          print(self.change(args))
+        elif program == "save":
+          print(self.save(args))
+        elif program == "load":
+          print(self.load(args))
+        
+        # Om kommandot inte finns
+        else:
+          print("Invalid command")
 
-      # Hantera quit
-      if program == "quit":
-        break
+      # Om inga argument finns
+      else:
+        print("Invalid use or command")
+
 
   def add(self, args):
     # Lägg in value arg2 i key arg1 i self.book
     # Orkar inte skapa variabler så detta får vara lite halft oläsbart
     if args[0] and args[1] and not self.book.get(args[0]):
       try:
-        # Gör om nummret till en int
+        # Gör om numret till en int
         num = int(args[1])
         self.book[args[0]] = num
         return f"Added entry for {args[0]}: {args[1]}"
@@ -44,6 +59,32 @@ class Phonebook:
       if result:
         # Om resultatet är en sträng, genomför en ny sökning med resultatet som sträng, annars returna resultatet
         return type(result) == str and self.performLookup(result) or result
+
+      # Hantera avsaknat resultat
+      else:
+        return None
+
+    # Hantera ogiltig söksträng
+    else:
+      return None
+
+  # Fungerar på samma sätt som performLookup fast ändrar värdet istället för att returna det
+  def performChange(self, search, newVal):
+    if search:
+      result = self.book.get(search)
+      if result:
+        # Om resultatet är en sträng, genomför en ny sökning med resultatet som sträng, annars ändra värdet
+        if type(result) == str:
+          self.performChange(result, newVal)
+        else:
+          try:
+            # Gör om numret till en int
+            num = int(newVal)
+            self.book[search] = num
+          except:
+            return None
+        
+        return "Success"
 
       # Hantera avsaknat resultat
       else:
@@ -71,6 +112,61 @@ class Phonebook:
       return f"Added alias for {args[0]}: {args[1]}"
     else:
       return ":("
+
+  # Om det finns ett nummer för namnet, ändra det till det nya
+  def change(self, args):
+    # Dubbelkolla att argumenten och värdet finns
+    if args[0] and args[1] and self.performLookup(args[0]):
+      
+      # Försök ändra värdet och kolla hur det gick
+      if self.performChange(args[0], args[1]):
+        return f"Changed number for {args[0]}: {args[1]}"
+      else:
+        return ":("
+
+    else:
+      return ":("
+
+  # Spara den nuvarande tabellen/telefonboken till fil
+  def save(self, args):
+    # Kolla så att arg0 (filnamn) finns
+    if args[0]:
+      # Skapa en sträng som går att spara till filen som angetts
+      result = ""
+
+      # Lägg till varje värde i tabellen
+      for key in self.book:
+        val = self.performLookup(key)
+        result += f"{key};{val};\n" # Lägg till en ny rad med format: key;value;
+
+      # Spara till filen
+      with open(args[0], "w") as file:
+        file.write(result)
+        return f"Successfully saved data to {args[0]}"
+    
+    # Något gick fel
+    return ":("
+
+  # Ladda en tabell/telefonbok från fil
+  def load(self, args):
+    # Kolla så att arg0 (filnamn) finns
+    if args[0]:
+      # Ladda filen
+      with open(args[0], "r") as file:
+        saveData = file.read()
+        if saveData:
+          # Gå igenom och splitta upp strängen efter först \n, sedan ;
+          self.book = {} # Skapa en ny tabell
+          for line in saveData.split("\n"):
+            lineData = line.split(";")
+            # Skriv sedan in det i den nya tabellen
+            if lineData[0] and lineData[1]:
+              self.book[lineData[0]] = int(lineData[1])
+          
+          return f"Successfully loaded data from {args[0]}, {saveData.split('\n')} entries found"
+    
+    # Något gick fel
+    return ":("
 
 
 Phonebook().main()
