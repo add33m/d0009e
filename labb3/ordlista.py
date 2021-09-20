bcfdd9de049902ac38ae6f5abd2a7dbdb997384d
add33m <48681209+add33m@users.noreply.github.com>
2021-09-20 17:29:00 +0200

    Completed labb3

--- labb3/ordlista.py
+++ labb3/ordlista.py
@@ -1,6 +1,7 @@
 # Importera funktionen jag definierade i menu.py
 import menu
 
+# Använder dictionary
 def main_dic():
   dic = {} # Skapa dictionary med ordlistan
 
@@ -11,9 +12,14 @@ def main_dic():
     # WHYYYY kan inte python bara ha en switch case som ett vanligt jävla programmeringsspråk aaaaaaaaaaaaaaaaaaaaaaaaaaahhhhhhhhhhhhh
     if cmd == 1:
       key = str(input("Word: "))
-      val = str(input("Description: "))
-      dic[key] = val
-      print("Added word", "'" + key + "'", "with discription:", "'" + val + "'")
+      # Förhindra duplicates genom att kolla om ordet redan finns
+      if key in dic or key.lower() in dic:
+        print("Error: entry already exists for word '" + key + "'")
+      # Annars, lägg till ordet
+      else:
+        val = str(input("Description: "))
+        dic[key] = val
+        print("Added word", "'" + key + "'", "with discription:", "'" + val + "'")
 
     elif cmd == 2:
       key = str(input("Word: "))
@@ -29,4 +35,80 @@ def main_dic():
     elif cmd == 3:
       break
 
-main_dic()
+# Använder två listor av strängar
+# Denna funktion söker inte med lowercase då detta kräver mer jobb med error handling att implementera
+def main_dic2():
+  # Skapa två listor där ordlistan lagras
+  keys = []
+  vals = []
+
+  while True:
+    print("--- Menu for dictionary ---")
+    cmd = menu.presentMenu(["Insert", "Lookup", "Exit"])
+    
+    # WHYYYY kan inte python bara ha en switch case som ett vanligt jävla programmeringsspråk aaaaaaaaaaaaaaaaaaaaaaaaaaahhhhhhhhhhhhh
+    if cmd == 1:
+      key = str(input("Word: "))
+      # Förhindra duplicates genom att kolla om ordet redan finns
+      if key in keys:
+        print("Error: entry already exists for word '" + key + "'")
+      
+      # Annars, lägg till ordet
+      else:
+        val = str(input("Description: "))
+        keys.append(key)
+        vals.append(val)
+        print("Added word", "'" + key + "'", "with discription:", "'" + val + "'")
+
+    elif cmd == 2:
+      key = str(input("Word: "))
+      # Visa ordets beskrivning om den finns
+      if key in keys:
+        index = keys.index(key)
+        print("Description for word", "'" + key + "':", vals[index])
+      
+      # Visa felmeddelande om beskrivning inte finns
+      else:
+        print("No description found for word '" + key + "'")
+
+    # Avsluta funktionen genom att bryta den oändliga while-loopen
+    elif cmd == 3:
+      break
+
+# Använder en lista av tupler
+def main_dic3():
+  dic = []
+
+  while True:
+    print("--- Menu for dictionary ---")
+    cmd = menu.presentMenu(["Insert", "Lookup", "Exit"])
+    
+    # WHYYYY kan inte python bara ha en switch case som ett vanligt jävla programmeringsspråk aaaaaaaaaaaaaaaaaaaaaaaaaaahhhhhhhhhhhhh
+    if cmd == 1:
+      key = str(input("Word: "))
+      # Förhindra duplicates genom att kolla om ordet redan finns
+      if list(filter(lambda t: t[0] == key, dic)):
+        print("Error: entry already exists for word '" + key + "'")
+
+      # Annars, lägg till ordet
+      else:
+        val = str(input("Description: "))
+        dic.append((key, val))
+        print("Added word", "'" + key + "'", "with discription:", "'" + val + "'")
+
+    elif cmd == 2:
+      key = str(input("Word: "))
+      # Returnerar en array med alla tuples som matchar sökordet
+      tuples = list(filter(lambda t: t[0] == key, dic))
+      if tuples:
+        print("Description for word", "'" + key + "':", tuples[0][1]) # Ta beskrivningen från den första (enda) tuplen som hittats
+      
+      # Visa felmeddelande om beskrivning inte finns
+      else:
+        print("No description found for word '" + key + "'")
+
+    # Avsluta funktionen genom att bryta den oändliga while-loopen
+    elif cmd == 3:
+      break
+
+main_dic3()
